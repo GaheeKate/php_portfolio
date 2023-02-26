a58b333f8f9cf504791921f094af20297f85022b
Gahee Choi <gaheekate@gmail.com>
2023-02-25 20:50:18 -0500

    first commit

---
+++ admin/includes/database.php
@@ -0,0 +1,10 @@
+<?php
+
+$connect = mysqli_connect(
+    "sql109.epizy.com", // Host
+    "epiz_33366146", // Username
+    "XPqKajsUiP", // Password
+    "epiz_33366146_cms" // Database
+);
+
+mysqli_set_charset($connect, 'UTF8');
